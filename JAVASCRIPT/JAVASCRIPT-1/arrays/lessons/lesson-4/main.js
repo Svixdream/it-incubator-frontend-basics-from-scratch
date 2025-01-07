9135413d9cd159ef7a5f2871c1885851aa8ee514
Vladimir Ivanushkin <Svixdream@gmail.com>
2025-01-07 20:49:20 +0300

    feat: creating a function to calculate the average score

--- JAVASCRIPT/JAVASCRIPT-1/arrays/lessons/lesson-4/main.js
+++ JAVASCRIPT/JAVASCRIPT-1/arrays/lessons/lesson-4/main.js
@@ -44,12 +44,20 @@ console.log(scores5); */
 //$ Функция для расчета среднего балла
 function calculateAverage(scores) {
     let sum = 0 // Инициализируем переменную для суммы оценок
- 
+
     for (let i = 0; i < scores.length; i++) {
         sum += scores[i] // Добавляем каждую оценку к сумме
     }
- 
+
     // Возвращаем средний балл округленный до ближайшего целого
     return Math.round(sum / scores.length)
 }
+const myScores = [82, 75, 91, 85, 93, 88, 99]
+
+const average = calculateAverage(myScores)
+console.log(`Средний балл студента: ${average}`);
+
+
+//% Функция для перевода баллов в другую шкалу
+
 
